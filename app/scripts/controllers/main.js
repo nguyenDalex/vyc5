46eb99ec5e0e116a28dbad1d1b045ea443d67828
Dat Tran <dattran91@gmail.com>
2014-11-24 19:33:46 -0800

    json option

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,16 @@
  * Controller of the vyc5AngularApp
  */
 angular.module('vyc5AngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'AngularJS'
-    ];
-  });
+        .controller('MainCtrl', function($scope, $http) {
+                $scope.awesomeThings = [
+                        'AngularJS'
+                ];
+                $http.get('http://blog.vyc5.org/api/get_recent_posts/')
+                        .success(function(data) {
+                                $scope.recentPosts = data;
+                        })
+                        .error(function() {
+                                console.log('Failed to get recent posts');
+
+                        });
+        });
