1e5f105bbed8781ce5b7d05140c7c0e07ac92150
Dat Tran <Dat.Nguyen@nordstrom.com>
2014-12-10 09:07:13 -0800

    group sub navigation

--- app/scripts/controllers/group.js
+++ app/scripts/controllers/group.js
@@ -8,15 +8,24 @@
  * Controller of the vyc5AngularApp
  */
 angular.module('vyc5AngularApp')
-  .controller('GroupCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.menu = [
-    {
-    	'title':'Overview',
-    	'link':'/'
-    }];
-  });
+	.controller('GroupCtrl', function ($scope, $http) {
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+		$http({
+			method: 'GET',
+			url: 'http://groups.vyc5.org/api/get_page/?id=2&custom_fields=*',
+			headers: {
+				'Accept': 'application/json; charset=utf-8'
+			}
+		})
+		.success(function(data) {
+						$scope.group = data.page.custom_fields;
+		})
+		.error(function() {
+						console.log('Failed to get group');
+
+		});
+	});
